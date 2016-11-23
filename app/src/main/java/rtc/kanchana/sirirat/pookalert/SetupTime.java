dfa9f52e25938e54c66b2d1f02e229b781255791
masterUNG <phrombutr@gmail.com>
2016-11-23 16:56:01 +0700

    Finish 23nov

--- app/src/main/java/rtc/kanchana/sirirat/pookalert/SetupTime.java
+++ app/src/main/java/rtc/kanchana/sirirat/pookalert/SetupTime.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.CalendarView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class SetupTime extends AppCompatActivity {
 
     //Explicit
     private CalendarView calendarView;
+    private TimePicker timePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +21,23 @@ public class SetupTime extends AppCompatActivity {
         //Bind Widget
         calendarView = (CalendarView) findViewById(R.id.calendarView);
 
+
         //CalendarView Controller
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
 
                 Log.d("pookV1", "date ==> " + day + "/" + (month + 1) + "/" + year);
-
+                Toast.makeText(SetupTime.this, "คุณเลือก " + day + "/" + (month + 1) + "/" + year,
+                        Toast.LENGTH_SHORT).show();
 
             }   // onSelected
         });
 
 
     }   // Main Method
+
+
+
 
 }   // Main Class
