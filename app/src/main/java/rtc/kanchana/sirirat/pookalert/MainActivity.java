f2f3fb1c5902cc1dc329abdf17633b971de4281e
masterUNG <phrombutr@gmail.com>
2016-11-23 13:41:19 +0700

    Intent Settime finish

--- app/src/main/java/rtc/kanchana/sirirat/pookalert/MainActivity.java
+++ app/src/main/java/rtc/kanchana/sirirat/pookalert/MainActivity.java
@@ -1,7 +1,9 @@
 package rtc.kanchana.sirirat.pookalert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
         //Show View
         timeTextView.setText(timeString);
         dateTextView.setText(dateString);
+
+        //Image Controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SetupTime.class));
+            }
+        });
+
 
     }   // Main Method
 
